9b10417f2ae10b5b84eba99335c24a514f57b089
Author <tiengtrung30s@gmail.com>
2020-12-09 12:59:14 +0800

    added post page

---
+++ pages/post/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative min-h-screen">
+      <Navbar></Navbar>
+      <Footer class="absolute bottom-0"></Footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

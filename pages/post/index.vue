c7812e952f77a5e2f785735ec9f9e48f94fafceb
Author <tiengtrung30s@gmail.com>
2020-12-10 20:34:04 +0800

    populated post page

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="relative min-h-screen">
       <Navbar></Navbar>
+      <h1 class="text-center font-semibold text-3xl my-2
+                            sm:text-base ">Bài đăng</h1>
+      <div class="flex justify-center pb-40">
+        <div class ="grid grid-cols-4 gap-8
+                         xl:grid-cols-3 xl:gap-4 mt-8 
+                         md:grid-cols-3 md:gap-4 mt-8  
+                         sm:grid-cols-2 sm:gap-2
+                         xs:grid-cols-1 xs:gap-1">
+              <BlogPost v-for="b in blogs" :key="b.id" :post="b.Title" :content="b.Content" :url ="b.Cover[0].url"></BlogPost>
+        </div>
+      </div>
+      
       <Footer class="absolute bottom-0"></Footer>
   </div>
 </template>
@@ -8,7 +20,22 @@
 <script>
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import BlogPost from '../../components/BlogPost'
+import gql from 'graphql-tag'
 export default {
+  apollo:{
+        blogs: gql`query{
+            blogs(sort: "published_at:DESC"){
+                id,
+                published_at,
+                Title,
+                Content,
+                Cover{
+                    url
+                }
+            }
+        }`
+    },
 
 }
 </script>

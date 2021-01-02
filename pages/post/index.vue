1d3d49e627599c0d56462dc6e4da04dbfd992d44
Author <tiengtrung30s@gmail.com>
2021-01-02 22:24:33 +0800

    changed to 1 columns view

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="relative min-h-screen">
       <Navbar></Navbar>
-      <h1 class="text-center font-semibold text-3xl my-2
-                            sm:text-base ">Bài đăng</h1>
+      <h1 class="text-center font-semibold text-3xl my-2">Bài đăng</h1>
       <div class="flex justify-center pb-40">
         <div class ="grid grid-cols-4 gap-8
                          xl:grid-cols-3 xl:gap-4 mt-8 
                          md:grid-cols-3 md:gap-4 mt-8  
-                         sm:grid-cols-2 sm:gap-2
+                         sm:grid-cols-1 sm:gap-2
                          xs:grid-cols-1 xs:gap-1">
               <BlogPost v-for="b in blogs" :key="b._id" :title="b.title" :content="b.body" :url ="b.mainImage.asset._ref" route="post" :slug="b.slug.current"></BlogPost>
         </div>
--- pages/tai-lieu/index.vue
+++ pages/tai-lieu/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="relative min-h-screen">
       <Navbar></Navbar>
-      <h1 class="text-center font-semibold text-3xl my-2
-                            sm:text-base ">Tài Liệu</h1>
+      <h1 class="text-center font-semibold text-3xl my-2">Tài Liệu</h1>
       <div class="flex justify-center pb-40">
         <div class ="grid grid-cols-4 gap-8
                          xl:grid-cols-3 xl:gap-4 mt-8 
                          md:grid-cols-3 md:gap-4 mt-8  
-                         sm:grid-cols-2 sm:gap-2
+                         sm:grid-cols-1 sm:gap-2
                          xs:grid-cols-1 xs:gap-1">
               <BlogPost v-for="r in resources" :key="r._id" :title="r.name" :content="r.content" :url ="r.image.asset._ref" route="tai-lieu" :slug="r.slug.current"></BlogPost>
         </div>

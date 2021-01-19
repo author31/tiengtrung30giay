bf271ea3d25768ff6caa989f5ccbc5ef7f88b18a
Author <tiengtrung30s@gmail.com>
2021-01-19 19:28:31 +0800

    added sendgridmail

--- functions/emailSending.js
+++ functions/emailSending.js
@@ -3,11 +3,16 @@ sgMail.setApiKey(process.env.SENDGRID_API);
 
 exports.handler = async function(event, context, callback) {
     const data = JSON.parse(event.body);
+    console.log(data)
     const msg = {
         to: "authorwong31@gmail.com",
         from: data.email,
-        subject: `You have a new message from ${data.email}`,
-        message: data.message
+        templateId: "d-a0a242f61a5d4c53bebc863f22faca32",
+        dynamic_template_data: {
+            subject: `You have a new message from ${data.email}`,
+            message: data.message
+        }
+        
     }
     sgMail.send(msg)
     return{
